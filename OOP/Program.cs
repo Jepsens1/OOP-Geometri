fd466d7e97244504fe89b8a479229f8100de9c4b
Phillip Svanholt Kjær Jepsen <jepsens123@outlook.dk>
2021-02-22 09:29:02 +0100

    Add project files.

---
+++ OOP/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square sqaure = new Square();
+
+            Console.WriteLine(sqaure.OmkredsMethod());
+            Console.WriteLine(sqaure.ArealMethod());
+
+        }
+    }
+}
---
+++ OOP/Square.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    class Square
+    {
+        private int a;
+
+        public int A
+        {
+            get { return a; }
+            set { a = value; }
+        }
+        public Square()
+        {
+            a = 7;
+        }
+        public int OmkredsMethod()
+        {
+            return 4 * a;
+        }
+        public int ArealMethod()
+        {
+            return a * a;
+        }
+
+    }
+}
